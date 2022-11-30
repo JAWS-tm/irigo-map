0f99060edf7fa59f78991f173db6811e905388f7
Jules <jules.dempt@outlook.fr>
2022-11-30 23:50:53 +0100

    fix: resolve conflict & remove unused code

--- backend/src/main/java/fr/eseo/equipe2/pglback/service/UserService.java
+++ backend/src/main/java/fr/eseo/equipe2/pglback/service/UserService.java
@@ -10,6 +10,7 @@ import fr.eseo.equipe2.pglback.exception.ExceptionType;
 import fr.eseo.equipe2.pglback.model.PasswordResetToken;
 import fr.eseo.equipe2.pglback.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
