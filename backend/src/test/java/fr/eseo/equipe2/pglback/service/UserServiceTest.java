d979decf49134c89e9f033c5c998df26ed40e37c
Jules <jules.dempt@outlook.fr>
2022-11-30 23:51:31 +0100

    test(Backend): enhance test forgot/reset password

--- backend/src/test/java/fr/eseo/equipe2/pglback/service/UserServiceTest.java
+++ backend/src/test/java/fr/eseo/equipe2/pglback/service/UserServiceTest.java
@@ -1,20 +1,31 @@
 package fr.eseo.equipe2.pglback.service;
 
-import fr.eseo.equipe2.pglback.UserDaoTests;
+import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+import com.icegreen.greenmail.junit5.GreenMailExtension;
+import com.icegreen.greenmail.util.ServerSetupTest;
 import fr.eseo.equipe2.pglback.controller.request.ForgotPasswordRequest;
-import fr.eseo.equipe2.pglback.controller.request.RegisterRequest;
+import fr.eseo.equipe2.pglback.controller.request.PasswordResetRequest;
 import fr.eseo.equipe2.pglback.dao.PasswordResetTokenDao;
 import fr.eseo.equipe2.pglback.dao.UserDao;
 import fr.eseo.equipe2.pglback.enumeration.UserSex;
+import fr.eseo.equipe2.pglback.model.PasswordResetToken;
 import fr.eseo.equipe2.pglback.model.User;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.FixMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,6 +33,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserServiceTest {
     @LocalServerPort
     private int port;
@@ -35,7 +47,13 @@ class UserServiceTest {
     @Autowired
     private UserDao userDao;
 
+    @RegisterExtension
+    static GreenMailExtension greenMail = new GreenMailExtension(ServerSetupTest.SMTP)
+            .withConfiguration(GreenMailConfiguration.aConfig().withUser("test", "test"))
+            .withPerMethodLifecycle(false);
+
     @Test
+    @Order(1)
     void createDefaultUser() {
         userDao.save(new User(
                 "jules.dempt@outlook.fr",
@@ -48,22 +66,81 @@ class UserServiceTest {
     }
 
     @Test
-    void forgotPassword() {
+    @Order(2)
+    void forgotPassword() throws MessagingException, IOException {
+        String email = "jules.dempt@outlook.fr";
         ForgotPasswordRequest forgotPasswordRequest = new ForgotPasswordRequest();
-        forgotPasswordRequest.setEmail("jules.dempt@outlook.fr");
+        forgotPasswordRequest.setEmail(email);
 
         String res = restTemplate.postForObject("http://localhost:"+port+"/api/users/forgot-password", forgotPasswordRequest, String.class);
-        System.out.println(res);
+
         assertTrue(res.contains("OK"));
 
-        System.out.println(passwordResetTokenDao.getReferenceById(1));
+        // check mails
+        greenMail.waitForIncomingEmail(1);
+        MimeMessage[] receivedMessages = greenMail.getReceivedMessages();
+        assertEquals(1, receivedMessages.length);
+        MimeMessage message = receivedMessages[0];
+
+        assertEquals("Réinitialiser votre mot de passe", message.getSubject());
+        assertEquals(email, message.getAllRecipients()[0].toString());
     }
 
     @Test
+    @Order(3)
     void validatePasswordToken() {
+        PasswordResetToken passwordResetToken = passwordResetTokenDao.getReferenceById(1);
+
+        assertNotNull(passwordResetToken);
+
+        String res = restTemplate.getForObject("http://localhost:"+port+"/api/users/validate-password-token/"+passwordResetToken.getToken(), String.class);
+        System.out.println(res);
+        assertTrue(res.contains("OK"));
     }
 
     @Test
+    @Order(4)
     void updatePassword() {
+        PasswordResetToken passwordResetToken = passwordResetTokenDao.getReferenceById(1);
+        System.out.println(passwordResetToken.getToken());
+        PasswordResetRequest passwordResetRequest = new PasswordResetRequest();
+        passwordResetRequest.setToken(passwordResetToken.getToken())
+                .setPassword("newPass");
+        String res = restTemplate.postForObject("http://localhost:" + port + "/api/users/reset-password/", passwordResetRequest, String.class);
+        System.out.println(res);
+        assertTrue(res.contains("OK"));
     }
+
+    @Test
+    void validateUnknownPasswordToken() {
+        String res = restTemplate.getForObject("http://localhost:"+port+"/api/users/validate-password-token/afbafuabf", String.class);
+
+        assertTrue(res.contains("BAD_REQUEST"));
+    }
+
+
+    @Test
+    void validateOutdatedPasswordToken() {
+        User user = userDao.save(new User(
+                "test@test.fr",
+                "test",
+                "Jean",
+                "Test",
+                UserSex.MALE,
+                new Date()
+        ));
+
+        PasswordResetToken passwordResetToken = new PasswordResetToken();
+        passwordResetToken.setToken("abc");
+        passwordResetToken.setExpiryDate(new Date());
+        passwordResetToken.setUser(user);
+
+        passwordResetTokenDao.save(passwordResetToken);
+
+        String res = restTemplate.getForObject("http://localhost:" + port + "/api/users/validate-password-token/abc", String.class);
+
+        assertTrue(res.contains("BAD_REQUEST"));
+    }
+
+
 }
